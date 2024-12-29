25d3992c4dcff309d6d454d2053e6e88add95a16
punzzl <161965350+punzzl@users.noreply.github.com>
2024-12-29 18:15:35 -0300

    Delete dist directory

--- dist/listeners/client/ready.d.ts
+++
@@ -1,5 +0,0 @@
-import { Listener } from '@sapphire/framework';
-export declare class ReadyEvent extends Listener {
-    run(): void;
-}
-//# sourceMappingURL=ready.d.ts.map
--- dist/structures/BotClient.d.ts
+++
@@ -1,7 +0,0 @@
-import { SapphireClient } from '@sapphire/framework';
-import { ClientOptions } from 'discord.js';
-export declare class BotClient extends SapphireClient {
-    constructor(options: ClientOptions);
-    start(): void;
-}
-//# sourceMappingURL=BotClient.d.ts.map
